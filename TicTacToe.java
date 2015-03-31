ef3c701bfa4c2dea862cff28534fc63f67c71928
bweisiger15 <bweisiger15@14ac02mtw49g083.punahou.edu>
2015-03-31 11:47:41 -1000

    Commit 2
    I have started coding the players

--- TicTacToe.java
+++ TicTacToe.java
@@ -8,9 +8,9 @@ public class TicTacToe
   public static void main( String[] args )
   {
     Scanner keyboard = new Scanner(System.in);
-    
     initialBoard();
     displayBoard();
+    getInfo();
     
   }
   
@@ -25,12 +25,72 @@ public class TicTacToe
   
   public static void displayBoard()
   {
-    System.out.println("  1  " + board[0][0] + "|" + board[0][1] + "|" + board[0][2]);
+    System.out.println("  0  " + board[0][0] + "|" + board[0][1] + "|" + board[0][2]);
     System.out.println("    --+-+--");
-    System.out.println("  2  " + board[1][0] + "|" + board[1][1] + "|" + board[1][2]);
+    System.out.println("  1  " + board[1][0] + "|" + board[1][1] + "|" + board[1][2]);
     System.out.println("    --+-+--");
-    System.out.println("  3  " + board[2][0] + "|" + board[2][1] + "|" + board[2][2]);
-    System.out.println("     1 2 3 ");
+    System.out.println("  2  " + board[2][0] + "|" + board[2][1] + "|" + board[2][2]);
+    System.out.println("     0 1 2 ");
+    System.out.println("--------------------------------------------------------");
+  }
+  //use scanner for user's desired row/col
+  //place their label in that row/col
+  
+  
+  
+  public static void getInfo()
+  {
+    System.out.println("Enter the row (0, 1 or 2)");
+    Scanner input = new Scanner(System.in);
+    int row = input.nextInt();
+    System.out.println("Enter the column (0, 1 or 2)");
+    Scanner input1 = new Scanner(System.in);
+    int col = input1.nextInt();
+    
+    if(board[row][col] != 'X' && board[row][col] != 'O')  
+    {
+      board[row][col] = 'X';
+      
+    }
+    else 
+    {
+      System.out.println("Someone already went there! Choose another space!");
+      getInfo();
+    }
+    System.out.println("Your last move:");
+    displayBoard();
+    compPlayer();
+  }
+  public static void compPlayer()
+  {
+    if(board[1][1] != 'X' && board[1][1] != 'O')  
+    {
+      board[1][1] = 'O';
+      
+    }
+    else if(board[0][0] != 'X' && board[0][0] != 'O')  
+    {
+      board[0][0] = 'O';
+      
+    }
+    else if(board[2][2] != 'X' && board[2][2] != 'O')  
+    {
+      board[2][2] = 'O';
+      
+    }
+    else if(board[0][2] != 'X' && board[0][2] != 'O')  
+    {
+      board[0][2] = 'O';
+      
+    }
+    else if(board[2][0] != 'X' && board[2][0] != 'O')  
+    {
+      board[2][0] = 'O';
+      
+    }
+    System.out.println("Computer's last move:");
+    displayBoard();
+    getInfo();
   }
   
   
